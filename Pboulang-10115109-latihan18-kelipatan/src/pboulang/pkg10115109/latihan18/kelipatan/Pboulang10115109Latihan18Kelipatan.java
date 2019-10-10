83ae05284135e5400f0b9754e182166ce1041e17
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-10-10 20:06:34 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan18-kelipatan/src/pboulang/pkg10115109/latihan18/kelipatan/Pboulang10115109Latihan18Kelipatan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan18.kelipatan;
+
+/**
+ *
+ * @author User
+ */
+public class Pboulang10115109Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float i;
+        float a = (float) 3.5;
+        
+       for (i=a;i<=35;i=(float) (i+3.5))
+        
+        
+        {
+            System.out.println(i);
+        }
+         System.out.println("");
+        System.out.println("  develop BY: indri yuliyanti  ");
+        
+    }
+    
+}
